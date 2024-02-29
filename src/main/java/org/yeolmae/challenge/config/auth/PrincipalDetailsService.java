47b64329f611ec73170efe624eed4b6a02c3e77a
thdrkfka <ssongjiaran@gmail.com>
2024-02-29 18:59:07 +0900

    회원가입, 로그인, 로그아웃 기능

--- src/main/java/org/yeolmae/challenge/config/auth/PrincipalDetailsService.java
+++ src/main/java/org/yeolmae/challenge/config/auth/PrincipalDetailsService.java
@@ -1,10 +1,7 @@
 package org.yeolmae.challenge.config.auth;
 
-import lombok.extern.log4j.Log4j2;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -12,9 +9,6 @@ import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 import org.yeolmae.challenge.domain.Member;
 import org.yeolmae.challenge.repository.MemberRepository;
-
-import java.util.Collections;
-import java.util.Optional;
 
 // 시큐리티 설정에서 loginProcessionUrl("/login");
 // /login 요청이 오면 자동으로 UserDetailsService 타입으로 IoC되어 있는 loadUserByUsername 함수가 실행
